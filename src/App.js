941273d4da4b795c72c914d83690f12cec843f31
Abdulrahman Bahyan <d7mii.b@gmail.com>
2022-12-13 18:43:37 +0300

    create service section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import About from './pages/About';
 import Home from './pages/Home';
+import Services from './pages/Services';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Services />
     </div>
   );
 }
