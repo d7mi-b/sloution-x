c85c415fa586207e24a7b3f2a0d2e0154a940161
Abdulrahman Bahyan <d7mii.b@gmail.com>
2023-02-18 20:00:18 +0300

    create API for contact us section

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,19 +13,25 @@ const Contact = () => {
                     </Typography>
                 </header>
 
-                <Box component="form" noValidate autoComplete="off" className={style.formContent}>
+                <Box 
+                    action="https://fabform.io/f/HDIt9Of"
+                    method='POST'
+                    component="form" 
+                    autoComplete="off" 
+                    className={style.formContent}
+                >
                     <section className={style.contnet}>
                         <section className={style.input}>
                             <label htmlFor='name'>الأسم</label>
-                            <TextField name='name' type='text' required size="small" gutterBottom />
+                            <TextField required name='name' type='text' size="small" gutterBottom />
                         </section>
                         <section className={style.input}>
                             <label htmlFor='phone'>رقم الجوال</label>
-                            <TextField name='phone' type='tel' size="small" gutterBottom />
+                            <TextField required name='phone' type='tel' size="small" gutterBottom />
                         </section>
                         <section className={style.input}>
                             <label htmlFor='email'>البريد الألكتروني</label>
-                            <TextField name='email' type='email' required size="small" gutterBottom />
+                            <TextField required name='email' type='email' size="small" gutterBottom />
                         </section>
                         <section className={style.input}>
                             <label htmlFor='service'>الخدمة المطلوبة</label>
@@ -37,7 +43,7 @@ const Contact = () => {
                         <TextareaAutosize name='idea' type='text' required size="small" gutterBottom  />
                     </section>
                     <section className={style.buttonContainer}>
-                        <Button variant="contained" endIcon={<SendIcon />} dir='ltr' gutterBottom>إرسال</Button>
+                        <Button type='submit' variant="contained" endIcon={<SendIcon />} dir='ltr' gutterBottom>إرسال</Button>
                     </section>
                 </Box>
             </article>
